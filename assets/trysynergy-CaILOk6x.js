782f582cb5bd40a4bae35930459d03f6e0695d0f
KnightboxOC <knazir88@gmail.com>
2026-04-19 02:53:45 +0100

    Discord Redirect Added and Site Update

---
+++ assets/trysynergy-CaILOk6x.js
@@ -0,0 +1,1 @@
+import{c as e,l as t,s as n,u as r}from"./zap-CbPiERv6.js";import{t as i}from"./TrySynergyPortal-Ce13FHR1.js";var a=r(t(),1),o=e(),s=n();(0,o.createRoot)(document.getElementById(`root`)).render((0,s.jsx)(a.StrictMode,{children:(0,s.jsx)(i,{onReturn:()=>{window.location.href=`./index.html`}})}));

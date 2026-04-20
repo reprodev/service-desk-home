0bbf8b1153a894ad8ddf1c136169149f40eaafce
KnightboxOC <knazir88@gmail.com>
2026-04-20 13:42:43 +0100

    Menu and UI fixes for the overall page

---
+++ assets/ascension-52rFdhy4.js
@@ -0,0 +1,1 @@
+import{c as e,l as t,s as n,u as r}from"./zap-B1IZrT-S.js";import{t as i}from"./SynergyPortal-BYntaceG.js";var a=r(t(),1),o=e(),s=n();(0,o.createRoot)(document.getElementById(`root`)).render((0,s.jsx)(a.StrictMode,{children:(0,s.jsx)(`div`,{className:`bg-black min-h-screen`,children:(0,s.jsx)(i,{onReturn:()=>{window.location.href=`./index.html`}})})}));
---
+++ assets/trysynergy-CDSyDk_b.js
@@ -0,0 +1,1 @@
+import{c as e,l as t,s as n,u as r}from"./zap-B1IZrT-S.js";import{t as i}from"./TrySynergyPortal-D5zpJoB8.js";var a=r(t(),1),o=e(),s=n();(0,o.createRoot)(document.getElementById(`root`)).render((0,s.jsx)(a.StrictMode,{children:(0,s.jsx)(i,{onReturn:()=>{window.location.href=`./index.html`}})}));
